259bf1ecd0f60c9c48057fec5e659241cb8563a4
Christie Reindle <creindle@hawaii.edu>
2017-06-30 08:44:42 -1000

    Completed super-secret

---
+++ app.js
@@ -0,0 +1,136 @@
+function resizeWindow() {
+  var width = window.innerWidth - 40,
+      height = window.innerHeight - 40;
+  
+  var canvas = document.getElementsByTagName('canvas')[0];
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+};
+
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.width = canvas.width + "px";
+canvas.style.height = canvas.height + "px";
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = document.createElement("img");
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "https://raw.github.com/lostdecade/simple_canvas_game/master/images/background.png";
+
+// Hero image
+var heroReady = false;
+var heroImage = document.createElement("img");
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "https://raw.github.com/lostdecade/simple_canvas_game/master/images/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = document.createElement("img");
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "https://raw.github.com/lostdecade/simple_canvas_game/master/images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+var monstersCaught = 0;
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+function reset() {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+function update(modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+// Draw everything
+function render() {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+};
+
+// The main game loop
+function main() {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+};
+
+// Let's play this game!
+reset();
+var then = Date.now();
+setInterval(main, 1); // Execute as fast as possible
